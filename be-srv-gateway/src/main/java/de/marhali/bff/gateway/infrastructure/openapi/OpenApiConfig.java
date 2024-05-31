d7900a3b94f151fb7f80c9759eff6e7e8b69b4f3
marhali <me@marhali.de>
2024-05-31 22:59:34 +0200

    also track gateway endpoints

--- be-srv-gateway/src/main/java/de/marhali/bff/gateway/infrastructure/openapi/OpenApiConfig.java
+++ be-srv-gateway/src/main/java/de/marhali/bff/gateway/infrastructure/openapi/OpenApiConfig.java
@@ -1,6 +1,6 @@
 package de.marhali.bff.gateway.infrastructure.openapi;
 
-import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties;
+import org.springdoc.core.properties.AbstractSwaggerUiConfigProperties.SwaggerUrl;
 import org.springdoc.core.properties.SwaggerUiConfigProperties;
 import org.springdoc.core.utils.Constants;
 import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -16,12 +18,17 @@ public class OpenApiConfig {
 	@Bean
 	@Primary
 	public SwaggerUiConfigProperties openApiUrlsLocatorViaRouteDefinitions(SwaggerUiConfigProperties properties, RouteDefinitionLocator routeDefinitionLocator) {
-		Set<AbstractSwaggerUiConfigProperties.SwaggerUrl> urls = routeDefinitionLocator.getRouteDefinitions()
+		Set<SwaggerUrl> urls = new HashSet<>();
+
+		var servicesSwaggerUrls = routeDefinitionLocator.getRouteDefinitions()
 			.filter(routeDefinition -> routeDefinition.getId().matches(".*-service"))
 			.map(routeDefinition -> {
-				var serviceName = routeDefinition.getId().replaceAll("-service", "");
-				return new AbstractSwaggerUiConfigProperties.SwaggerUrl(serviceName, "/" + serviceName + Constants.DEFAULT_API_DOCS_URL, null);
-		}).collect(Collectors.toSet()).block();
+				var serviceName = routeDefinition.getId().replace("-service", "");
+				return new SwaggerUrl(serviceName, "/" + serviceName + Constants.DEFAULT_API_DOCS_URL, null);
+			}).collect(Collectors.toSet()).block();
+
+		urls.add(new SwaggerUrl("gateway", Constants.DEFAULT_API_DOCS_URL, null));
+		urls.addAll(Objects.requireNonNull(servicesSwaggerUrls));
 
 		properties.setUrls(urls);
 		properties.getCsrf().setEnabled(true);
