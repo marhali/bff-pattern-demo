e8aacc342107f0ce4da12cc691d9d54feca7eb36
marhali <me@marhali.de>
2024-06-01 23:21:44 +0200

    feat: do not follow redirects

--- fe-app-spa/src/core/api/api-client.ts
+++ fe-app-spa/src/core/api/api-client.ts
@@ -9,16 +9,35 @@ const csrfHeaders = {
 
 const apiClient = {
   get: (url: string): Promise<Response> => {
-    return fetch(url, { method: 'GET', headers: { ...defaultHeaders }})
+    return fetch(url, {
+      method: 'GET',
+      headers: { ...defaultHeaders },
+      redirect: 'error'
+    })
   },
   post: (url: string, body?: unknown): Promise<Response> => {
-    return fetch(url, { method: 'POST', headers: { ...defaultHeaders, ...csrfHeaders }, body: body ? JSON.stringify(body) : undefined })
+    return fetch(url, {
+      method: 'POST',
+      headers: { ...defaultHeaders, ...csrfHeaders },
+      body: body ? JSON.stringify(body) : undefined,
+      redirect: 'error'
+    })
   },
   put: (url: string, body?: unknown): Promise<Response> => {
-    return fetch(url, { method: 'PUT', headers: { ...defaultHeaders, ...csrfHeaders }, body: body ? JSON.stringify(body) : undefined })
+    return fetch(url, {
+      method: 'PUT',
+      headers: { ...defaultHeaders, ...csrfHeaders },
+      body: body ? JSON.stringify(body) : undefined,
+      redirect: 'error'
+    })
   },
   delete: (url: string, body?: unknown): Promise<Response> => {
-    return fetch(url, { method: 'DELETE', headers: { ...defaultHeaders, ...csrfHeaders }, body: body ? JSON.stringify(body) : undefined })
+    return fetch(url, {
+      method: 'DELETE',
+      headers: { ...defaultHeaders, ...csrfHeaders },
+      body: body ? JSON.stringify(body) : undefined,
+      redirect: 'error'
+    })
   }
 }
 
