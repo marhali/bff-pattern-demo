e44cc4ec6333908eb87a0efcb6c4372b3c54aa61
marhali <me@marhali.de>
2024-06-02 20:33:30 +0200

    feat: also logout idp on logout

--- be-srv-gateway/src/main/java/de/marhali/bff/gateway/infrastructure/springboot/WebFluxSecurityConfig.java
+++ be-srv-gateway/src/main/java/de/marhali/bff/gateway/infrastructure/springboot/WebFluxSecurityConfig.java
@@ -1,11 +1,15 @@
 package de.marhali.bff.gateway.infrastructure.springboot;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.client.oidc.web.server.logout.OidcClientInitiatedServerLogoutSuccessHandler;
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.RedirectServerAuthenticationSuccessHandler;
+import org.springframework.security.web.server.authentication.logout.ServerLogoutSuccessHandler;
 import org.springframework.security.web.server.csrf.CookieServerCsrfTokenRepository;
 import org.springframework.security.web.server.csrf.CsrfToken;
 import org.springframework.web.server.WebFilter;
@@ -13,7 +17,11 @@ import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
+@RequiredArgsConstructor
 public class WebFluxSecurityConfig {
+
+	private final ReactiveClientRegistrationRepository clientRegistrationRepository;
+
 	@Bean
 	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
 		var cookieTokenRepository = CookieServerCsrfTokenRepository.withHttpOnlyFalse();
@@ -25,6 +33,7 @@ public class WebFluxSecurityConfig {
 				.csrfTokenRequestHandler(new ServerCsrfTokenHeaderRequestHandler()))
 			.authorizeExchange(authorize -> authorize.anyExchange().authenticated())
 			.oauth2Login(oauth2 -> oauth2.authenticationSuccessHandler(new RedirectServerAuthenticationSuccessHandler("/..")))
+			.logout(logout -> logout.logoutSuccessHandler(oidcLogoutSuccessHandler()))
 			.build();
 	}
 
@@ -34,5 +43,13 @@ public class WebFluxSecurityConfig {
 			exchange.getAttributeOrDefault(CsrfToken.class.getName(), Mono.empty()).subscribe();
 			return chain.filter(exchange);
 		};
+	}
+
+	private ServerLogoutSuccessHandler oidcLogoutSuccessHandler() {
+		OidcClientInitiatedServerLogoutSuccessHandler oidcClientInitiatedServerLogoutSuccessHandler = new OidcClientInitiatedServerLogoutSuccessHandler(this.clientRegistrationRepository);
+
+		oidcClientInitiatedServerLogoutSuccessHandler.setPostLogoutRedirectUri("http://localhost:5173");
+
+		return oidcClientInitiatedServerLogoutSuccessHandler;
 	}
 }
